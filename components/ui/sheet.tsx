1e43e2dfaca671e1dc32625ee6f0c3cd54277bd8
Raj Gupta <98741542+opcxder@users.noreply.github.com>
2023-10-14 01:35:25 +0530

    Update sheet.tsx

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -14,11 +14,6 @@ const SheetTrigger = SheetPrimitive.Trigger
 const SheetClose = SheetPrimitive.Close
 
 const SheetPortal = ({
-<<<<<<< HEAD
-
-=======
- 
->>>>>>> c704bafaad2e22ecd11f827c358a95c07eafefc0
   ...props
 }: SheetPrimitive.DialogPortalProps) => (
   <SheetPrimitive.Portal  {...props} />
