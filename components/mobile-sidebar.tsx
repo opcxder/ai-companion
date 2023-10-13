cb59cc7599caf618166a0bb4873b040ee0c126cc
Raj <visionatoz07@gmail.com>
2023-10-14 01:11:39 +0530

    fix prop passing issue in sidebar

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -6,15 +6,18 @@ import {
     SheetTrigger,
 } from '@/components/ui/sheet';
 import { Sidebar } from "@/components/sideBar";
+import { boolean } from "zod";
 
-export const MobileSideBar = () => {
+export const MobileSideBar = ({ isPro }: {
+    isPro: boolean;
+}) => {
     return (
         <Sheet>
-            <SheetTrigger  className="md:hidden pr-4">
-                <Menu /> 
+            <SheetTrigger className="md:hidden pr-4">
+                <Menu />
             </SheetTrigger>
             <SheetContent side="left" className="p-0  bg-secondary  pt-10  w-32">
-                <Sidebar/>
+                <Sidebar isPro={isPro} />
             </SheetContent>
         </Sheet>
     )
